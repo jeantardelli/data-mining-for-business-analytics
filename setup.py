8bb033fe26e8a93d1614124f35fe83a719fa87f2
jeantardelli <jeantardelli@gmail.com>
2021-10-12 17:07:32 +0200

    Classification and Regression Trees - Pt. I

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dmutils",
-    version="0.0.1",
+    version="0.0.2",
     description="A userful module for the book exercises and analysis",
     packages=find_packages("src"),
     package_dir={"" : "src"},
--- src/dmutils/graphs.py
+++ src/dmutils/graphs.py
@@ -4,7 +4,19 @@ Analytics" book. The code you find here is adapted from:
 
     https://github.com/gedeck/dmba/blob/master/src/dmba/featureSelection.py
 """
+import io
 import pandas as pd
+import numpy as np
+from sklearn.tree import export_graphviz
+
+try:
+    from IPython.display import Image
+except ImportError:
+    Image = None
+try:
+    import pydotplus
+except ImportError:
+    pydotplus = None
 
 
 def gains_chart(gains, color='C0', label=None, ax=None, figsize=None):
@@ -33,6 +45,7 @@ def gains_chart(gains, color='C0', label=None, ax=None, figsize=None):
     ax.set_xlabel('# records')
     ax.set_ylabel('cumulative gains')
     return ax
+
 
 def lift_chart(predicted, title='Decile Lift Chart', label_bars=True, ax=None, figsize=None):
     """
@@ -63,3 +76,80 @@ def lift_chart(predicted, title='Decile Lift Chart', label_bars=True, ax=None, f
         for p in ax.patches:
             ax.annotate('{:.1f}'.format(p.get_height()), (p.get_x(), p.get_height() + 0.1))
     return ax
+
+
+def plot_decision_tree(decision_tree, feature_names=None, class_names=None, impurity=False,
+                       label='root', max_depth=None, rotate=False, pdf_file=None):
+    """
+    Create a plot of the scikit-learn decision tree and show in the Jupyter notebook.
+    Function adapted from the https://github.com/gedeck/dmba/blob/master/src/dmba/graphs.py
+
+    Input:
+        decision_tree: scikit-learn decision tree
+        feature_names (optional): variable names
+        class_names (optional): class names, only relevant for classification trees
+        impurity (optional): show node impurity
+        label (optional): only show labels at the root
+        max_depth (optional): limit
+        rotate (optional): rotate the layout of the graph
+        pdf_file (optional): provide a pathname to create a PDF file of the graph
+    """
+    if pydotplus is None:
+        return 'You need to install pydotplus to visualize decision trees.'
+    if Image is None:
+        return 'You need to install ipython to visualize decision trees.'
+    if class_names is not None:
+        class_names = [str(s) for s in class_names] # convert to strings
+    dot_data = io.StringIO()
+    export_graphviz(decision_tree, feature_names=feature_names, class_names=class_names,
+                    impurity=impurity, label=label, out_file=dot_data, filled=True,
+                    rounded=True, special_characters=True, max_depth=max_depth, rotate=rotate)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    if pdf_file is not None:
+        graph.write_pdf(str(pdf_file))
+    return Image(graph.create_png())
+
+
+# Taken from scikit-learn documentation
+
+def text_decision_tree(decision_tree, indent='  ', as_ratio=True):
+    """
+    Create a text representation of the scikit-learn decisioin tree.
+    Input:
+         decision_tree: scikit-learn decision tree
+         as_ratio: show the composition of the leaf nodes as ratio (default)
+                   instead of counts.
+         indent: indentation (default two spaces)
+    """
+    n_nodes = decision_tree.tree_.node_count
+    children_left = decision_tree.tree_.children_left
+    children_right = decision_tree.tree_.children_right
+    feature = decision_tree.tree_.feature
+    threshold = decision_tree.tree_.threshold
+    node_value = decision_tree.tree_.value
+
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, -1)] # seed is the root node id and its parent depth
+    while len(stack) > 0:
+        node_id, parent_depth = stack.pop()
+        node_depth[node_id] = parent_depth + 1
+        # If we have a test node
+        if (children_left[node_id] != children_right[node_id]):
+            stack.append((children_left[node_id], parent_depth + 1))
+            stack.append((childre_right[node_id], parent_depth + 1))
+        else:
+            is_leaves[node_id] = True
+
+    rep = []
+    for i in range(n_nodes):
+        common = f'{node_depth[i] * indent}node={i}'
+        if is_leaves[i]:
+            value = node_value[i]
+            if as_ratio:
+                value = [[round(vi  / sum(v), 3) for vi in v] for v in value]
+            rep.append(f'{common} leaf node: {value}')
+        else:
+            rule = f'{children_left[i]} if {feature[i]} <= {threshold[i]} else to node {children_right[i]}'
+            rep.append(f'{common} test node: got to node {rule}')
+    return '\n'.join(rep)
